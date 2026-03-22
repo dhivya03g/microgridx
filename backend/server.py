606f0bbcee263b1b1449f30f611e810a31c681c1
dhivya03g <9123243012@kcgcollege.com>
2026-03-22 15:41:42 +0530

    added flask to requirements

--- backend/server.py
+++ backend/server.py
@@ -41,4 +41,4 @@ def get_data():
 
 # 🔥 Run server (accessible to frontend)
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000)
